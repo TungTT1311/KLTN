d88db0293d51a8a6c8b8d9074e9f735a14b04e2d
TungTT <17521241@gm.uit.edu.vn>
2021-04-27 15:59:39 +0700

    Tung up

---
+++ pedestrian_detection/object_detection.h
@@ -0,0 +1,26 @@
+#ifndef OBJECT_DETECTION_H
+#define OBJECT_DETECTION_H
+#pragma once
+#include "opencv2/core/utility.hpp"
+#include <vector>
+#include <string>
+#include "gpu.h"
+#include "mat.h"
+#include "net.h"
+#include <iostream>
+struct Object{
+    cv::Rect rec;
+    float prob;
+};
+
+class ObjectDetection
+{
+    public:
+        ObjectDetection(const std::string _model_path);
+        std::vector<cv::Rect> detectObject(const cv::Mat& _frame);
+	~ObjectDetection();
+    private:
+	ncnn::Net mobilenet;
+};
+
+#endif
---
+++ pedestrian_detection/test.cpp
@@ -0,0 +1,58 @@
+#include "object_detection.h"
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+
+int main( int argc, char** argv )
+{
+    std::string pipeline = "v4l2src device=/dev/video0 ! image/jpeg, width=(int)1280, height=(int)720, framerate=30/1 ! jpegdec ! videoconvert ! appsink";
+    cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
+    cv::Mat frame;
+
+    ObjectDetection det("../../pedestrian_detection/");
+    auto m_StartTime = std::chrono::system_clock::now();
+    while(true)
+    {
+        cap >> frame;
+	double fps = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - m_StartTime).count();
+	m_StartTime = std::chrono::system_clock::now();
+	cv::putText(frame, to_string(static_cast<int>(1000/fps)) + " FPS", cv::Point(10, 30), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 0, 255), 1, false);
+        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+	try{        
+		auto recs = det.detectObject(frame);
+		if (recs.empty()){
+			std:: cout<<"no person to detect"<<std::endl;
+		}
+		else{
+		 	std:: cout<<"point1 "<< recs[0]<< " point2 "<<recs[1]<<std::endl; 
+		}
+        	std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+        	std::chrono::duration<double> time_used = std::chrono::duration_cast<std::chrono::duration<double>>(t2-t1);
+        	//std::cout<<"Optical flow time: " << time_used.count() << "seconds" << std::endl;
+        	for(auto rec:recs)
+        	{
+            	cv::rectangle(frame, rec, cv::Scalar(0, 255, 0), 2, 1);
+        	}
+		throw 0;
+	}catch(int e)
+	{
+		if (e=0){
+		std:: cout<<"no person to detect"<<std::endl;
+	}
+	}
+	//std:: cout << recs[0]<<std::endl;
+	
+        cv::imshow("detection", frame);
+        if(cv::waitKey(1) == 27)
+        {
+            break;
+        }
+    }
+    cap.release();
+    cv::destroyAllWindows() ;
+    return 0;
+}
